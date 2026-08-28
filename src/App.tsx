93e87fc8078467a97de392ed17efb545cf6845c4
João Pedro <joaopedro.sc11110@gmail.com>
2026-08-28 19:06:18 -0300

    feat:adiciona componente galeria

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { Header } from "./components/Header";
-import { Hero } from "./components/Hero";
-
+import { Categories } from "./components/Categories"
+import { Gallery } from "./components/Gallery"
+import { Header } from "./components/Header"
+import { Hero } from "./components/Hero"
 
 function App() {
 
   return (
     <>
       <Header />
+
       <main className="py-10">
         <Hero />
+        <Categories />
+        <Gallery />
       </main>
     </>
   )
