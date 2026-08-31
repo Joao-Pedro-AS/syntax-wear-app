cfac31815aa18f0b02778ae391dcf3c1196dadc4
João Pedro <joaopedro.sc11110@gmail.com>
2026-08-31 10:28:01 -0300

    feat: Roteamento com TanStackRouter

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,17 +2,20 @@ import Logo from '@/assets/images/logo.png';
 import IconUser from '@/assets/images/icon-user.png';
 import IconCart from '@/assets/images/icon-cart.png';
 import IconAbout from '@/assets/images/icon-about.png';
+import { Link } from '@tanstack/react-router';
 
 export const Header = () => {
     return (
         <div className="relative">
             <header className="fixed top-10 left-0 right-0 z-10 mx-10">
-                <div className='bg-white max-w-330 mx-auto flex justify-between items-center py-3 px-7 rounded-2xl mt-5'>
+                <div className='bg-white text-black max-w-330 mx-auto flex justify-between items-center py-3 px-7 rounded-2xl mt-5'>
                     <img className='w-32 md:w-36' src={Logo} alt="Logo SyntaxWear" />
 
                     <nav className='hidden md:block'>
                         <ul className='flex gap-10'>
-                            <li><a href="#">Masculino</a></li>
+                            <li>
+                                <Link to="/products">Masculino</Link>
+                            </li>
                             <li><a href="#">Feminino</a></li>
                             <li><a href="#">Outlet</a></li>
                         </ul>
@@ -22,7 +25,11 @@ export const Header = () => {
                         <ul className='flex gap-10'>
                             <li className='hidden md:block'><a href="#">Nossas Lojas</a></li>
                             <li className='hidden md:block'><a href="#">Sobre</a></li>
-                            <li><a href="#"><img src={IconUser} alt="Ícone de Login" /></a></li>
+                            <li>
+                                <Link to="/sign-in">
+                                    <img src={IconUser} alt="Ícone de Login" />
+                                </Link>
+                            </li>
                             <li><a href="#"><img src={IconAbout} alt="Ícone de Sobre" /></a></li>
                             <li><a href="#"><img src={IconCart} alt="Ícone de Carrinho" /></a></li>
                         </ul>
