320c419c7a3cbf4b417905e2332ec55a980ed88f
João Pedro <joaopedro.sc11110@gmail.com>
2026-09-02 12:16:02 -0300

    feat: adiciona listagem de produtos

--- src/pages/_app/products/index.tsx
+++ src/pages/_app/products/index.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { ProductList } from '../../../components/ProductList'
+import { products } from '../../../mocks/products'
 
 export const Route = createFileRoute('/_app/products/')({
     component: RouteComponent,
@@ -6,14 +8,14 @@ export const Route = createFileRoute('/_app/products/')({
 
 function RouteComponent() {
     return (
-        <div className="container">
-            <h1 className='text-black'>OLÁ</h1>
+        <div className="container pt-44 md:pt-54 pb-10 md:px-10 mb-10 text-black bg-surface">
 
-            <p className='text-black'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis sed laudantium ab magnam nulla, ipsum alias asperiores praesentium optio illum aspernatur quo non ut laboriosam. Rem placeat quis suscipit est?</p>
-            <p className='text-black'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis sed laudantium ab magnam nulla, ipsum alias asperiores praesentium optio illum aspernatur quo non ut laboriosam. Rem placeat quis suscipit est?</p>
-            <p className='text-black'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis sed laudantium ab magnam nulla, ipsum alias asperiores praesentium optio illum aspernatur quo non ut laboriosam. Rem placeat quis suscipit est?</p>
-            <p className='text-black'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis sed laudantium ab magnam nulla, ipsum alias asperiores praesentium optio illum aspernatur quo non ut laboriosam. Rem placeat quis suscipit est?</p>
-            <p className='text-black'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis sed laudantium ab magnam nulla, ipsum alias asperiores praesentium optio illum aspernatur quo non ut laboriosam. Rem placeat quis suscipit est?</p>
+            <h1 className="text-3xl text-center mb-3">Lista de Produtos</h1>
+
+            <h2 className="text-center text-2xl mb-10 p-4">Conforto excepcional para suas aventuras no dia-a-dia</h2>
+
+            <ProductList products={products} />
+
         </div>
     )
 }
